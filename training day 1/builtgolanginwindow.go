46aa74be13449a34d5aa5d784e2b4972a95025fa
rikkei-chiennm <chiennm@rikkeisoft.com>
2022-04-28 17:07:15 +0700

    build goLang program

---
+++ training day 1/builtgolanginwindow.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		cach cai dat golang tren cac nen tang khac nhau
+		https://topdev.vn/blog/hoc-golang-tu-con-so-0-phan-1-cai-dat-golang-tren-linux-va-windows/#cai-dat-golang-tren-ubuntu
+		còn về viết chương trình đầu tiên thì nó như phần ở dưới đây
+		- package
+		- import thư viện
+		- main
+		- code trong main
+	*/
+
+	fmt.Println("hello word")
+
+}
