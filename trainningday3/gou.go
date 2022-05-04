8b5192956fabcabffb204e86a1c3ac656d0ed584
rikkei-chiennm <chiennm@rikkeisoft.com>
2022-05-04 14:55:17 +0700

    gourotine basic

--- trainningday3/gou.go
+++ trainningday3/gou.go
@@ -1,1 +1,43 @@
-package trainningday3
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	var wg = sync.WaitGroup{}
+	wg.Add(2)
+
+	fmt.Println("trainning goLang day 3")
+
+	go func() {
+
+		count("sleep")
+		wg.Done()
+		//sau khi chay toi done thi chuong trinh chay xuong wg,wait
+
+	}()
+
+	go func() {
+		count("fish")
+		wg.Done()
+	}()
+
+	wg.Wait()
+	fmt.Println("DONE")
+	// khi có 2 gou thì chuong trình không in ra gì
+	// vì bản thân main khởi tạo một gou khi đó gọi ra 2 gou kia xong thì tự đóng chương trình
+
+	//time.Sleep(time.Second * 10)
+	// khi để chương trình đợi thì bắt gou main đợi rồi chạy 2 go trên
+
+}
+func count(name string) {
+	for i := 1; i <= 5; i++ {
+		fmt.Println(name, i)
+		time.Sleep(time.Second)
+	}
+}
