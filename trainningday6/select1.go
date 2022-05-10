abaf9e17a04a67c505bee1b643ed309fc6a755de
rikkei-chiennm <chiennm@rikkeisoft.com>
2022-05-10 09:32:30 +0700

    select basic in golang

---
+++ trainningday6/select1.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+/*
+The select statement is used to choose from multiple send/receive channel operations.
+The select statement blocks until one of the send/receive operations is ready.
+If multiple operations are ready, one of them is chosen at random.
+The syntax is similar to switch except that each of the case statements will be a channel operation.
+*/
+func main() {
+	ex1 := make(chan string)
+	ex2 := make(chan string)
+	go example1(ex1)
+	go example2(ex2)
+	select {
+	case s1 := <-ex1:
+		fmt.Println(s1)
+	case s2 := <-ex2:
+		fmt.Println(s2)
+	}
+
+}
+func example1(ex1 chan string) {
+	time.Sleep(3 * time.Second)
+	ex1 <- "run ex1"
+}
+func example2(ex2 chan string) {
+	time.Sleep(1 * time.Second)
+	ex2 <- "run ex2"
+}
