7c38ca12cc3000d7b262fae8483483ef32e596e6
rikkei-chiennm <chiennm@rikkeisoft.com>
2022-05-10 15:09:31 +0700

    ticker

---
+++ trainningday6/exticker.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ticker := time.NewTicker(500 * time.Millisecond)
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case _ = <-ticker.C:
+				fmt.Println("Ticker not stop")
+
+			}
+		}
+	}()
+	time.Sleep(2000 * time.Millisecond)
+	done <- true
+	fmt.Println("program done")
+}
